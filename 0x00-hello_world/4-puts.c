89d649325aed93c82e1b9115fe9312447461dc39
laschien <laschienstudy@gmail.com>
2023-03-16 16:21:23 -0700

    Add the 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mystr[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(mystr);
+
+	return (0);
+}
