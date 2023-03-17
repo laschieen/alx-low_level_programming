9216daeb725bde4fd3626a7c20a1a90aff2f8d9d
laschien <laschienstudy@gmail.com>
2023-03-16 17:38:18 -0700

    Fix the error in the 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,18 +3,18 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints a message to the standard error using the write() function
- * without using the printf() family or puts() family of functions.
+ * Description: Prints a message to the standard error using the write()
+ * function without using the printf() family or puts() family of functions.
  *
  * Return: Always returns 1 to indicate an error occurred.
  */
 int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const int message_size = sizeof(message) - 1; /* exclude the terminating null character */
+	const char message[] =
+	    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const int message_size = sizeof(message) - 1;
 
-    write(STDERR_FILENO, message, message_size);
+	write(STDERR_FILENO, message, message_size);
 
-    return (1);
+	return (1);
 }
-
