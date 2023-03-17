beb75f2168966972049bf73d1b1c5521784024a7
laschien <laschienstudy@gmail.com>
2023-03-17 16:47:29 -0700

    Add the 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = {"abcdefghijklmnopqrstuvwxyz"};
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabet[i] == 'q' || alphabet[i] == 'e')
+			continue;
+
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
