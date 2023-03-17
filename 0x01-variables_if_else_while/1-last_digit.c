f5c092a4683dbf9be4bfb231e108984251736842
laschien <laschienstudy@gmail.com>
2023-03-17 16:10:56 -0700

    Fix the error in the 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit = n % 10;
+	int last_digit;
+	last_digit = n % 10;
+
 	if (last_digit > 5)
 		printf("Last digit of %i is %i and is greater then 5\n", n, last_digit);
 	else if (last_digit == 0)
