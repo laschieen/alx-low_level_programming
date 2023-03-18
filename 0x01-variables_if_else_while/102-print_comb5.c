c23cc53db09d4843c7733311fbc9794f5678d4ce
laschien <laschienstudy@gmail.com>
2023-03-17 18:53:13 -0700

    Edit the 102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two two-digit numbers
+ * main - Entry point
+ * Description: Prints all possible combinations of two two-digit numbers.
+ * The numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits. 1 should be printed as 01
+ * The combination of numbers must be separated by comma, followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * 00 01 and 01 00 are considered as the same combination
+ of the numbers 0 and 1
+ * You can only use the putchar function (every other function is forbidden)
+ * You can only use putchar eight times maximum in your code
+ * You are not allowed to use any variable of type char
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			int tens1 = i / 10;
-			int ones1 = i % 10;
-			int tens2 = j / 10;
-			int ones2 = j % 10;
-
-			putchar(tens1 + '0');
-			putchar(ones1 + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar(tens2 + '0');
-			putchar(ones2 + '0');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 
-			if (i != 99 || j != 99)
+			if (!(i == 98 && j == 99))
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-
+	putchar('\n');
 	return (0);
 }
+
