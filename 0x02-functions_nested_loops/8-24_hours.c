97d117ad240fbec423b88cc38aba05c1f3f59cf9
laschien <laschienstudy@gmail.com>
2023-03-21 18:38:22 -0700

    Add the 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_baucer - prints every minute of the day of Jack Bauer
+ * jack_baucer - prints every minute of the day.
  *
  * Description: prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59.
