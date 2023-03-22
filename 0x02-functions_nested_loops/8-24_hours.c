6d742fb7c591ef897610fc17984006d29a39a27e
laschien <laschienstudy@gmail.com>
2023-03-21 18:33:54 -0700

    Fix 7-print_last_digit file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_baucer - prints every minute of the day of Jack Bauer
+ *
+ * Description: prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59.
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+				}
+			}
+		}
+	}
+}
