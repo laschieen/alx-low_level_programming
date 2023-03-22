f4efda2b628b0855448a4c74fb6b6fe2976ee4f9
laschien <laschienstudy@gmail.com>
2023-03-21 17:46:46 -0700

    Fix the error in the 7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,12 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = -n % 10;
+	}
 
 	_putchar('0' + last_digit);
 	return (last_digit);
