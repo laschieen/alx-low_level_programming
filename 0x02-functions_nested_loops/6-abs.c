0697db7c7c1cfecd5ba5372b3d4498c0e2286c20
laschien <laschienstudy@gmail.com>
2023-03-21 17:18:18 -0700

    Fix the error in the 6-abs.c file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,16 +2,16 @@
 
 /**
  * _abs - computes the absolute value of an integer.
- *@int: the integer to be processed.
+ *@n: the integer to be processed.
  *
  * Return: the absolute value of the int
  */
-int _abs(int)
+int _abs(int n)
 {
-	if (int < 0)
+	if (n < 0)
 	{
-		return (-int);
+		return (-n);
 	}
 
-	return (int);
+	return (n);
 }
